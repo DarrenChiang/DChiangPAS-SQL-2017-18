6faae269653169ddcd337a60352a9b320d391f36
DarrenChiang <nerradchiang@gmail.com>
2018-01-29 00:03:41 +0800

    Assignment-1
    I tried to recreate the site through tables and basic stuff and failed spectacularly. Copy-pasting the html from the site works just fine though (as seen in test.php).

---
+++ Assignment-1/assignment-1.php
@@ -0,0 +1,90 @@
+<DOCTYPE html>
+<html>
+<head>
+<style>
+table#1 {border-collapse: collapse;}
+table#2 {text-align: left;}
+body {font-family: Helvetica;}
+</style>
+</head>
+<body>
+<table>
+</table>
+<br><br>
+<p style="text-align:center;"><img src="https://livedemo00.template-help.com/wt_41989/images/slide1.jpg"></p>
+<br><br>
+<table id="1" style="width:100%">
+	<tr>
+		<th style="border-right: solid 1px #f00;">Home</th>
+		<th style="border-right: solid 1px #f00;">About Us</th>
+		<th style="border-right: solid 1px #f00;">Projects</th>
+		<th style="border-right: solid 1px #f00;">Solutions</th>
+		<th style="border-right: solid 1px #f00;">Blog</th>
+		<th>Contacts</th>
+</table>
+<br><br><br>
+<hr>
+<br><br><br>
+<table id="2" style="width:100%">
+	<tr>
+		<th><h1>What we do?</h1></th>
+		<th><h1>Welcome</h1></th>
+		<th><h1>What's new?</h1></th>
+	</tr>
+	<tr>
+		<td rowspan="3">
+			<ul>
+				<li>Praesent vestibulum molestie lacus</li>
+				<li>Aenean no nummy hendr</li>
+				<li>Phasellus porta. Fusce suscipit varius</li>
+				<li>Cum sociis atoque penat ibus et magnis</li>
+				<li>Parturien duiusce feugiat male</li>
+				<li>Suada odioorbi nunc</li>
+				<li>Odio gravida atcursus nec luc</li>
+				<li>Maecenas tristique orci ac sem</li>
+				<li>Duis ult ricies pharetra magnaonec</li>
+			</ul>
+		</th>
+		<td rowspan="3">
+			<p>
+				<b>Lorem ipsum dolor sit amet consect</b><br>
+				Praesent vestibulum molestie lacus. Aean no<br>
+				nummy hendrerit mauris. Phasllus porta. Fusce<br>
+				suscipit varius mi. Cum sociis atoqat ibus et magnis<br>
+				dis parturien dui.
+				<br>
+				<b>Fusce feugiat malesuada odio.</b><br>
+				Morbi nunc odio gravida atcursus nec luctus lorem.<br>
+				Maecenas tristique orcic sem. Duis ult ricies<br>
+				pharetra magna. Donec acumn male suada orci.<br>
+				Donec sit amet eros.
+			</p>
+		</th>
+		<td>
+			<p>
+				AUG 18, 2012<br>
+				Praesent vestibulum molestie lacan no numrta.<br>
+				Fusce suscipit varius mi.<br><hr>
+			</p>
+		</td>
+	<tr>
+		<td>
+			<p>
+				SEPT 21, 2012<br>
+				Praesent vestibulum molestie lacan no numrta.<br>
+				Fusce suscipit varius mi.<br><hr>
+			</p>
+		</td>
+	</tr>
+	<tr>
+		<td>
+			<p>
+				OCT 09, 2012<br>
+				Praesent vestibulum molestie lacan no numrta.<br>
+				Fusce suscipit varius mi.
+			</p>
+		</td>
+	</tr>
+</table>
+</body>
+</html>
